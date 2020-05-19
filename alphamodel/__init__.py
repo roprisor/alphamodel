272ca9431cc74ab076bcf1d9558760c16cb72af4
Razvan <fir3blad3r@gmail.com>
2020-05-18 22:14:37 -0400

    Add BLHMM to init

--- alphamodel/__init__.py
+++ alphamodel/__init__.py
@@ -3,3 +3,4 @@ from .scenario import Scenario
 from .ss_ewm import SingleStockEWM
 from .ss_expost import SingleStockExPost
 from .ss_hmm import SingleStockHMM
+from .ss_bl_hmm import SingleStockBLHMM
